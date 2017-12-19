44d3366b9de124f84817b33b3ea9c62836a8d1ce
Anggar Wahyu <anggarwahyu@gmail.com>
2017-12-19 18:56:26 +0700

    Add box in menu

--- core.c
+++ core.c
@@ -3,6 +3,7 @@
 #include <windows.h>
 #include <conio.h>
 #include <time.h>
+#include <pthread.h>
 
 #define REP(i, k) for(int i=0; i<k; i++)
 #define REPP(i, j, k) REP(i, k) REP(j, k)
@@ -22,6 +23,7 @@ int FloodedVertically[WIDTH][WIDTH];
 long long Score = 0;
 long long Point = 0;
 long long localHighscore  = 0;
+int globalCounter = 0;
 
 void GenerateBox(), GenerateCleanBox(), MainMenu();
 
@@ -157,6 +159,44 @@ void SidepanelOne(){
     REP(i, 10) REP(j, 22){ GotoXY(OFFSET+j+1, i+1); printf(" ");}
 }
 
+void ControlHelp(){
+    SetColor(7);
+
+    REP(i, 25){
+        GotoXY(i+OFFSET, 12);
+        if(i == 0)   { printf("%c", 213); continue;}
+        if(i == 24)  { printf("%c", 184); continue;}
+        printf("%c", 205);
+    }
+
+    REP(i, 25){
+        GotoXY(i+OFFSET, 26);
+        if(i == 0)   { printf("%c", 192); continue;}
+        if(i == 24)  { printf("%c", 217); continue;}
+        printf("%c", 196);
+    }
+
+    REP(j, 13){
+        GotoXY(OFFSET, j+13);
+        printf("%c", 179);
+
+        GotoXY(OFFSET+24, j+13);
+        printf("%c", 179);
+    }
+
+    GotoXY(OFFSET, 2+12);  printf("%c     C O N T R O L     %c", 179, 179);
+    GotoXY(OFFSET, 4+12);  printf("%c", 195); REP(i, 23) printf("%c", 196); printf("%c", 180);
+    
+    GotoXY(OFFSET, 6+12);  printf("%c [ARROW] FOR MOVEMENT  %c", 179, 179);
+    GotoXY(OFFSET, 8+12);  printf("%c [SPACE] SELECT GEM    %c", 179, 179);
+    GotoXY(OFFSET, 9+12);  printf("%c         FOR SWAPPING  %c", 179, 179);
+    GotoXY(OFFSET, 11+12); printf("%c [ENTER] SELECT MENU   %c", 179, 179);
+    GotoXY(OFFSET, 13+12); printf("%c [ESC]   BACK TO MENU  %c", 179, 179);
+    GotoXY(OFFSET, 7+12);  printf("%c                       %c", 179, 179);
+    GotoXY(OFFSET, 10+12); printf("%c                       %c", 179, 179);
+    GotoXY(OFFSET, 12+12); printf("%c                       %c", 179, 179);
+}
+
 void HighScore(){
     // BOX DRAWING
 
@@ -183,6 +223,8 @@ void HighScore(){
         GotoXY(OFFSET+24, j+13);
         printf("%c", 179);
     }
+
+    REP(i, 13){ GotoXY(OFFSET, i+1+12);  printf("%c                       %c", 179, 179);}
 
     // DATA RETRIEVING AND WRITING
 
@@ -238,6 +280,8 @@ void HighScore(){
 }
 
 void Help(){
+    ControlHelp();
+
     SidepanelOne();
 
     GotoXY(OFFSET, 1);   printf("%c               H E L P %c", 179, 179);
@@ -245,10 +289,10 @@ void Help(){
     GotoXY(OFFSET, 4);   printf("%c CONSECUTIVE GEMS      %c", 179, 179);
     GotoXY(OFFSET, 5);   printf("%c BY SWAPPING THEM      %c", 179, 179);
 
-    GotoXY(OFFSET, 7);    printf("%c SCORE IS ALL GEMS     %c", 179, 179);
-    GotoXY(OFFSET, 8);    printf("%c YOU GOT DURING GAME   %c", 179, 179);
-    GotoXY(OFFSET, 9);    printf("%c POINT IS GEMS YOU GET %c", 179, 179);
-    GotoXY(OFFSET, 10);   printf("%c IN LAST SWAP SESSION   %c", 179, 179);
+    GotoXY(OFFSET, 7);   printf("%c SCORE IS ALL GEMS     %c", 179, 179);
+    GotoXY(OFFSET, 8);   printf("%c YOU GOT DURING GAME   %c", 179, 179);
+    GotoXY(OFFSET, 9);   printf("%c POINT IS GEMS YOU GET %c", 179, 179);
+    GotoXY(OFFSET, 10);  printf("%c IN LAST SWAP SESSION  %c", 179, 179);
 
     if(getch() == 27) MainMenu();
     else Help();
@@ -283,9 +327,11 @@ void CurrentScore(){
 }
 
 void MainMenu(){
-    SidepanelOne();
+    pthread_t tid;
 
     HighScore();
+
+    SidepanelOne();
 
     GotoXY(OFFSET, 2); printf("%c   G E M A M B A N G   %c", 179, 179);
     GotoXY(OFFSET, 3); printf("%c                       %c", 179, 179);
@@ -299,6 +345,14 @@ void MainMenu(){
     SHORT textPos = 0;
 
     while(1){
+        REPP(i, j, WIDTH){
+        Board[i][j] = (((i+j)/2+globalCounter) % 5) + 1;
+        }
+
+        GenerateBox();
+        globalCounter++;
+        SetColor(15);
+
         switch(getch()){
             case 224:
                 switch(getch()){
@@ -604,7 +658,7 @@ void Movement(){
                 break;
             case 27:
                 localHighscore = Score;
-                HighScore();
+                ClearSelectionBox(SelectionCursorX, SelectionCursorY);
                 MainMenu();
                 break;
             case 224:
